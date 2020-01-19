8549a94c41bb5be2522a794af6a68bf9c851c3d0
Christian Leicht <christian@leicht.io>
2020-01-19 20:04:26 +0100

    fix: updated demo-app.

--- examples/app.ts
+++ examples/app.ts
@@ -6,7 +6,7 @@ export class App {
     }
 
     startKadmos(): void {
-        // Example: http://localhost:1234/?fileUrl=https://ni.leicht.io/bg_1000_handle.stl&color=0xeeeeee
+        // Example: http://localhost:1234/?fileUrl=https://ni.leicht.io/bg_1000_handle.stl&color=0x333333
         Kadmos.initFromUrl();
     }
 }
